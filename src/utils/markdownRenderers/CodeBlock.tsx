61d6eaba328a1553beb9b81f815c52b02f75188a
Cristian Ramirez <ramireze4@gmail.com>
2025-11-11 19:49:10 -0600

    apply format

--- src/utils/markdownRenderers/CodeBlock.tsx
+++ src/utils/markdownRenderers/CodeBlock.tsx
@@ -1,10 +1,10 @@
-import React, {PropsWithChildren} from 'react';
+import React, { PropsWithChildren } from 'react';
 
-import {PrismLight as SyntaxHighlighter} from 'react-syntax-highlighter';
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
 import css from 'react-syntax-highlighter/dist/esm/languages/prism/css';
 import js from 'react-syntax-highlighter/dist/esm/languages/prism/javascript';
 import ts from 'react-syntax-highlighter/dist/esm/languages/prism/typescript';
-import {oneDark} from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 import styles from './CodeBlock.module.css';
 
