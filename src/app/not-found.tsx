3bf50580ad80f4bec8d28133f8c108d2ada9bde0
Cristian Ramirez <ramireze4@gmail.com>
2025-11-15 15:43:30 -0600

    add custom 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+export default function NotFound() {
+  return (
+    <main>
+      <div className="not-found-container">
+        <h1>404</h1>
+        <div>
+          <p>This page could not be found.</p>
+        </div>
+      </div>
+    </main>
+  );
+}
